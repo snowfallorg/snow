12a860ebe973c567c1ef8df8446cbbe6de96ee90
Victor Fuentes <vmfuentes64@gmail.com>
2023-02-07 15:42:13 -0500

    Update nix files

---
+++ default.nix
@@ -0,0 +1,32 @@
+{ pkgs ? import <nixpkgs> { }
+, lib ? import <nixpkgs/lib>
+}:
+pkgs.rustPlatform.buildRustPackage {
+  pname = "snow";
+  version = "0.0.2";
+
+  src = [ ./. ];
+
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+    outputHashes = {
+      "nix-data-0.0.2" = "sha256-yts2bkp9cn4SuYPYjgTNbOwTtpFxps3TU8zmS/ftN/Q=";
+    };
+  };
+
+  nativeBuildInputs = with pkgs; [
+    makeWrapper
+    pkg-config
+  ];
+  buildInputs = with pkgs; [
+    openssl
+  ];
+
+  doCheck = false;
+
+  postInstall = ''
+    wrapProgram $out/bin/snow --prefix PATH : '${pkgs.lib.makeBinPath [ pkgs.sqlite ]}'
+    mkdir -p $out/libexec
+    mv $out/bin/snow-helper $out/libexec/snow-helper
+  '';
+}
--- flake.nix
+++ flake.nix
@@ -1,38 +1,30 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    naersk.url = "github:nix-community/naersk";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
+    utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, naersk, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, utils, ... }:
+    utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        naersk-lib = naersk.lib."${system}";
+        name = "snow";
       in
+      rec
       {
-        packages.snow = naersk-lib.buildPackage {
-          pname = "snow";
-          root = ./.;
-          nativeBuildInputs = with pkgs; [ makeWrapper ];
-          buildInputs = with pkgs; [
-            openssl
-            pkg-config
-            sqlite
-          ];
-          postInstall = ''
-            wrapProgram $out/bin/snow --prefix PATH : '${pkgs.lib.makeBinPath [ pkgs.sqlite ]}'
-            mkdir -p $out/libexec
-            mv $out/bin/snow-helper $out/libexec/snow-helper
-          '';
+        packages.${name} = pkgs.callPackage ./default.nix {
+          inherit (inputs);
         };
 
-        defaultPackage = self.packages.${system}.snow;
+        # `nix build`
+        defaultPackage = packages.${name};
+
+        # `nix run`
+        apps.${name} = utils.lib.mkApp {
+          inherit name;
+          drv = packages.${name};
+        };
+        defaultApp = apps.${name};
 
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
