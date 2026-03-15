da139d43eabf55ad5e606409649746279ca9af0d
Victor Fuentes <vlinkz@snowflakeos.org>
2026-03-14 22:00:43 -0700

    bump libsnow

--- packages/snow/default.nix
+++ packages/snow/default.nix
@@ -18,7 +18,7 @@ rustPlatform.buildRustPackage {
   cargoLock = {
     lockFile = ../../Cargo.lock;
     outputHashes = {
-      "libsnow-0.0.2-alpha.1" = "sha256-0GVxbiExLCHsz3rzfM+WPiEonXfgMzB7TWTPmDOY3uQ=";
+      "libsnow-0.0.2-alpha.1" = "sha256-JczgWRsUOjM9kbInRuUWJVgyMBk+PiA8SxAy6GgJUXQ=";
     };
   };
 
